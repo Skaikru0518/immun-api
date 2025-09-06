20100af3493f044e981f67dbfca203efa7a30041
Skaikru <dante0518@outlook.com>
2025-09-06 23:12:01 +0200

    disabled auto-sync for database

--- src/app.module.ts
+++ src/app.module.ts
@@ -23,7 +23,7 @@ import { ReceptorsModule } from './receptors/receptors.module';
         password: configService.get<string>('DB_PASSWORD'),
         database: configService.get<string>('DB_NAME'),
         autoLoadEntities: true,
-        synchronize: true,
+        synchronize: false,
       }),
     }),
     CytokinesModule,
